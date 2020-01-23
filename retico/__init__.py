86e5939655359b82655ef0aef64d8dc572c5be8e
V!nc3r <Vinc3r@users.noreply.github.com>
2020-01-23 20:45:43 +0100

    wiki doc

--- retico/__init__.py
+++ retico/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "description": "RealTime Companion, a 3D realtime workflow addon oriented.",
     "author": "Vincent (V!nc3r) Lamy, CStfan",
     "category": "3D View",
-    "wiki_url": 'https://github.com/Vinc3r/ReTiCo#Documentation',
+    "wiki_url": 'https://github.com/Vinc3r/ReTiCo/wiki/Documentation',
     "tracker_url": 'https://github.com/Vinc3r/ReTiCo/issues',
     "version": (2020, 1, 23),
     "blender": (2, 81, 0)
