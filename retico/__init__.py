218803e268ce275ff8de73e4a237ac6ab791bf55
Vincent Lamy <Vinc3r@users.noreply.github.com>
2020-02-12 10:37:07 +0100

    in-dev date

--- retico/__init__.py
+++ retico/__init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "category": "3D View",
     "wiki_url": 'https://github.com/Vinc3r/ReTiCo/wiki#retico',
     "tracker_url": 'https://github.com/Vinc3r/ReTiCo/issues',
-    "version": (2020, 1, 24),
+    "version": (2020, 2, 12),
     "blender": (2, 81, 0)
 }
 
